2c76dd77233ed73b8652111b8ff79a678eaf74ac
searky <searky@163.com>
2020-08-30 22:56:25 +0800

    update SerializerUtil.java

--- wRaft/src/main/java/com/github/searky/wKeyValueDB/wRaft/Serializer/SerializerUtil.java
+++ wRaft/src/main/java/com/github/searky/wKeyValueDB/wRaft/Serializer/SerializerUtil.java
@@ -1,4 +1,17 @@
 package com.github.searky.wKeyValueDB.wRaft.Serializer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Serializer Utility
+ */
 public class SerializerUtil {
+    private static final Logger logger = LoggerFactory.getLogger(SerializerUtil.class);
+    private static Map<Class<?>/* Object Class */, Serializer<?>/* Serializer Implement */> serializerMap = new HashMap<>();
+    private static Map<Byte/* Type of Serializer */, Class<?>/* Object Class */> typeMap = new HashMap<>();
+
 }
